a5ff26956bbd026fc7f0b2a8ccd49ed5bc50f5c5
carol198 <carolina.fernandez.vela@hotmail.com>
2016-07-15 07:49:30 -0400

    otros cambios con jquery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 $('.dropdown-toggle').dropdown()
 
 $(document).ready(function(){
-	$("div").children();
-	}
+    $("#terror").click(function(){
+        $("#1").hide();
+        $("#2").hide();
+        $("#3").hide();
+        $("#4").show();
+        $("#5").show();
+        $("#6").show();
+        $("#7").show();
+        $("#8").show();
+        $("#9").show();
+    });
+    $("#suspenso").click(function(){
+        $("#1").show();
+        $("#2").show();
+        $("#3").show();
+        $("#4").hide();
+        $("#5").hide();
+        $("#6").hide();
+        $("#7").hide();
+        $("#8").hide();
+        $("#9").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#1").click(function(){
+        $("div #panel").toggle();
+    });
+});
